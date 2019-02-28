2b05f92129696ebaf9086c0c3df4d85ca12d696d
Saionaro <shibakow@gmail.com>
2019-02-28 14:34:47 +0300

    Updated critical

--- build.js
+++ build.js
@@ -1,33 +1,33 @@
-const fs = require('fs');
-const critical = require('critical');
-const { localized } = require('./dist/app.bundle.js');
+const fs = require("fs");
+const critical = require("critical");
+const { localized } = require("./dist/app.bundle.js");
 
-const langs = localized.map((item) => {
+const langs = localized.map(item => {
   fs.writeFileSync(`./dist/index.${item.code}.html`, item.content);
   return item.code;
 });
 
 const getSettings = lang => ({
-  base: 'dist/',
+  base: "dist/",
   src: `index.${lang}.html`,
-  dest: `${lang === 'ru' ? '' : `${lang}/`}index.html`,
+  target: {
+    css: "styles.css",
+    html: `${lang === "ru" ? "" : `${lang}/`}index.html`
+  },
   extract: true,
   inline: true,
-  minify: true,
-  css: [`dist/styles.css`],
+  minify: true
 });
 
-const remove = path =>
-  fs.unlink(path, () =>
-    console.log(`Removed: ${path}`));
+const remove = path => fs.unlink(path, () => console.log(`Removed: ${path}`));
 
-Promise.all(
-  langs.map(lang => critical.generate(getSettings(lang)))
-).then(() => {
-  const toRemove = [
-    './dist/app.bundle.js',
-    './dist/styles.css',
-    ...langs.map(lang => `./dist/index.${lang}.html`)
-  ];
-  toRemove.forEach(remove);
-});
+Promise.all(langs.map(lang => critical.generate(getSettings(lang)))).then(
+  () => {
+    const toRemove = [
+      "./dist/app.bundle.js",
+      "./dist/styles.css",
+      ...langs.map(lang => `./dist/index.${lang}.html`)
+    ];
+    toRemove.forEach(remove);
+  }
+);
