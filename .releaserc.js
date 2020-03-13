63aeab949c299defa7f256b4faafc7a80b6fe810
Saionaro <shibakow@gmail.com>
2020-03-13 18:02:58 +0300

    feat(release): Moving to npm and using semantic-release

---
+++ .releaserc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  branches: ["master"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          {
+            path: "release.zip",
+            label: "Website Bundle <%= nextRelease.name %>"
+          }
+        ]
+      }
+    ],
+    "@semantic-release/git"
+  ]
+};
