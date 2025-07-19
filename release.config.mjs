8ea5d8b231e69680ae57349912baab51554ec94f
Artem Shibakov <shibakow@gmail.com>
2025-07-19 16:56:29 +0400

    mjs

---
+++ release.config.mjs
@@ -0,0 +1,28 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ["master"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          {
+            path: "release.zip",
+            label: "Website Bundle <%= nextRelease.name %>",
+          },
+        ],
+      },
+    ],
+    "@semantic-release/git",
+  ],
+};
